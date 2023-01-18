a438afca913fba79c3f02495702f0d1ca5f9fd6d
fujisakikenya <fujisaki.k@dentsudigital.co.jp>
2023-01-18 16:46:26 +0900

    create VideoComponent

--- next/components/recipe /VideoComponent.tsx
+++ next/components/recipe /VideoComponent.tsx
@@ -1,33 +1,34 @@
-import React from "react";
-interface VideoComponentProps {
-  thumbnailUrl: string | undefined;
-  source: string | undefined;
-  type: string | undefined;
+import { FragmentType, useFragment } from "../../libs/gql";
+import { graphql } from "../../libs/gql/gql";
+
+const VideoComponent_Fragment = graphql(`
+  fragment VideoComponent_Fragment on Video {
+    thumbnailUrl
+    source
+    type
+  }
+`);
+
+export interface VideoComponentProps {
+  fragment: FragmentType<typeof VideoComponent_Fragment>;
 }
 
-export const VideoComponent = ({
-  thumbnailUrl,
-  source,
-  type,
-}: VideoComponentProps) => {
-  return (
+export const VideoComponent = (props: VideoComponentProps): JSX.Element => {
+  const fragment = useFragment(VideoComponent_Fragment, props.fragment);
+  return fragment.source && fragment.thumbnailUrl && fragment.type ? (
     <video
       width="560"
       height="560"
       preload="auto"
-      poster={thumbnailUrl}
-      controls={true}
+      poster={fragment.thumbnailUrl}
+      controls={true} //"controls"
       controlsList="nodownload"
-      muted={true}
-      className="native"
-      data-v-2932eb4e=""
+      muted={true} //"muted"
     >
-      <source src={source} type={type} data-v-049f9628="" />
-      <p data-v-049f9628="">
-        動画を再生するには、videoタグをサポートしたブラウザが必要です。
-      </p>
+      <source src={fragment.source} type={fragment.type} />
+      <p>動画を再生するには、videoタグをサポートしたブラウザが必要です。</p>
     </video>
+  ) : (
+    <></>
   );
 };
-
-export default VideoComponent;
